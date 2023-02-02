1de1430543dafd1df68aaf48533c037599261c93
DajkaMate <72004720+DajkaMate@users.noreply.github.com>
2023-02-02 13:29:53 +0100

    Add files via upload

--- 230131/edit_page.php
+++ 230131/edit_page.php
@@ -1,41 +1,47 @@
 <?php
     include("includes/header.php");
-    $name = ""; $desc = ""; $price = ""; $error = "";
+    $id = $_GET['id'];
+    $result = Id($id);
+    $row = mysqli_fetch_array($result);
+    $name = $row['name']; 
+    $desc = $row['desc']; 
+    $price = $row['price']; 
+    $error = "";    
+
     $mess = array("name" => "","desc" => "", "price" => "");
-    if(isset($_POST["add"])){  
-        if(empty($_POST["name"])){
+
+    if(isset($_POST["add"]))
+    {  
+        if(empty($_POST["name"]))
+        {
            $mess["name"] = "Empty field!";
         }
         else
         {
             $name = $_POST['name'];
-            $sv = Name($name);
-            if($sv > 0)
-            {
-                $mess['name'] = "Name Taken!";
-            }   
         }
-        if(empty($_POST["desc"])){
+        if(empty($_POST["desc"]))
+        {
             $mess["desc"] = "Empty field!";
         }
         else
         {
             $desc = $_POST['desc'];
         }
-        if(empty($_POST["price"])){
+        if(empty($_POST["price"]))
+        {
             $mess["price"] = "Empty field!";
         }
         else
         {
             $price = $_POST['price'];
         }
-
         if(array_filter($mess)){
             $error = true;
         } 
         else 
         {
-            Add($name, $desc, $price);
+            Edit($name, $desc, $price, $id);
             header("Location: success.php");
         }
     }
@@ -43,7 +49,7 @@
     <div class="container pb-2">
         <h1 class="text-center display-6 py-3">Add</h1>
         <div class="card m-auto" style="width: 20rem;">
-            <form action="" method="post">
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])."?id="."$id"?>" method="post">
                 <div class="card-body">
                     <div class="my-3">
                         <label for="name" class="form-label">Name</label>
--- 230131/includes/sql.php
+++ 230131/includes/sql.php
@@ -1,18 +1,45 @@
 <?php
-    function index()
-    {
+    function index(){
         $con = Connect();
-        $sql = "Select * from szendvicsek";
+        $sql = "SELECT * from szendvicsek";
         $result = mysqli_query($con,$sql);
         mysqli_close($con);
         return $result;
     }
 
+    function Name($name){
+        $con = Connect();
+        $sql = "SELECT * from szendvicsek where name like '$name'";
+        $result = mysqli_query($con,$sql);
+        $sv = mysqli_num_rows($result);
+        return $sv;
+        mysqli_close($con);
+    }   
+
+    function Id($id)
+    {
+        $con = Connect();
+        $sql = "SELECT * FROM szendvicsek WHERE id LIKE '$id'";
+        $result = mysqli_query($con, $sql);
+        return $result;
+        mysqli_close($con);
+    }
+
     function Add($name, $desc, $price){
         $con = Connect();
-        $sql = "INSERT INTO szendvicsek (name, desc, price) 
+        $sql = "INSERT INTO szendvicsek (`name`, `desc`, `price`) 
         VALUES('$name', '$desc', '$price')";
         mysqli_query($con, $sql);
         mysqli_close($con);
     }
+
+    function Edit($name, $desc, $price, $id)
+    {
+        $con = Connect();
+        $sql = "UPDATE szendvicsek SET `name` = '$name', `desc` = '$desc', `price` = '$price' where id = '$id'";
+        mysqli_query($con, $sql);
+        mysqli_close($con);
+    }
+
+    
 ?>
